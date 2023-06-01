dd9c0bc9217d770cbeaa49bd68ff548bcbe08c49
sakshya25 <2573st@gmail.com>
2023-06-01 19:13:12 +0530

    specialoffers

--- src/Components/Home/Components/SpecialOffer.jsx
+++ src/Components/Home/Components/SpecialOffer.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
-import './Style/SpecialOffer.css'
-const SpecialOffer = () => {
+import React from "react";
+import "./Style/SpecialOffer.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import "swiper/swiper-bundle.min.css";
+const SpecialOffer = (props) => {
   return (
-    <div>SpecialOffer</div>
-  )
-}
+    <>
+      <div className="container">
+        <div className="row">
+          <h1 className="special-offer-heading">Special offer</h1>
+        </div>
+        <div className="row special-offer-section">
+          
+          <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          // spaceBetween={50}
+          // slidesPerView={3}
+          navigation
+          pagination={{ clickable: true }}
+          // scrollbar={{ draggable: true }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+            breakpoints={{
+              0:{
+                slidesPerView:1,
+                spaceBetween:10,
+          
+              },
+              400:{
+                slidesPerView:3,
+                spaceBetween:20,
+          
+              },
+              720:{
+                slidesPerView:3,
+                spaceBetween:20,
+          
+              },
+              990:{
+                slidesPerView:3,
+                spaceBetween:40,
+          
+              }
+            }}
+          >
+            {props.data.map((item) => (
+              <SwiperSlide key={item.image} className="card-container">
+                <img src={item.linkedImg} className="special-offer-image " />
+                <div className="offers-intro">
+                  <h3 >{item.title}</h3>
+                  <p >{item.paragraph}</p>
+                  <span>{item.price}</span>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
--- src/Components/Home/Content/ContentSpecialOffer.jsx
+++ src/Components/Home/Content/ContentSpecialOffer.jsx
@@ -1,6 +1,54 @@
-const ContentSpecialOffer = {
-    
+ const ContentSpecialOffer = [
 
-};
+    {
+        id:1,
+        title:"Kerala River View Tour",
+        paragraph:"3 days | 4 Night | 20 Capacity",
+        price:"10%",
+        linkedImg:"https://webneel.com/wallpaper/sites/default/files/images/08-2018/3-nature-wallpaper-mountain.jpg",
+    },
+    {
+        id:2,
+        title:"Kerala River View Tour",
+        paragraph:"3 days | 4 Night | 20 Capacity",
+        price:"20%",
+        linkedImg:"https://webneel.com/wallpaper/sites/default/files/images/08-2018/3-nature-wallpaper-mountain.jpg",
+    },
+    {
+        id:3,
+        title:"Kerala River View Tour",
+        paragraph:"3 days | 4 Night | 20 Capacity",
+        price:"30%",
+        linkedImg:"https://webneel.com/wallpaper/sites/default/files/images/08-2018/3-nature-wallpaper-mountain.jpg",
+    },
+    {
+        id:4,
+        title:"Kerala River View Tour",
+        paragraph:"3 days | 4 Night | 20 Capacity",
+        price:"40%",
+        linkedImg:"https://webneel.com/wallpaper/sites/default/files/images/08-2018/3-nature-wallpaper-mountain.jpg",
+    },
+    {
+        id:5,
+        title:"Kerala River View Tour",
+        paragraph:"3 days | 4 Night | 20 Capacity",
+        price:"40%",
+        linkedImg:"https://webneel.com/wallpaper/sites/default/files/images/08-2018/3-nature-wallpaper-mountain.jpg",
+    },
+    {
+        id:6,
+        title:"Kerala River View Tour",
+        paragraph:"3 days | 4 Night | 20 Capacity",
+        price:"50%",
+        linkedImg:"https://webneel.com/wallpaper/sites/default/files/images/08-2018/3-nature-wallpaper-mountain.jpg",
+    }
+]
+    // Images:[
+    //     {img1:'https://webneel.com/wallpaper/sites/default/files/images/08-2018/3-nature-wallpaper-mountain.jpg'},
+    //     {img2:'https://webneel.com/wallpaper/sites/default/files/images/08-2018/3-nature-wallpaper-mountain.jpg'},
+    //     {img3:'https://webneel.com/wallpaper/sites/default/files/images/08-2018/3-nature-wallpaper-mountain.jpg'},
+        
+    // ]
+    export default ContentSpecialOffer;
+
   
-export default ContentSpecialOffer;
