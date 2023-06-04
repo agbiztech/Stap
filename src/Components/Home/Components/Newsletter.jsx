ed1abfdcb5c43210148dacfc2fe57b177781d176
sakshya25 <2573st@gmail.com>
2023-06-05 02:26:36 +0530

    newsletter and special offer completed

--- src/Components/Home/Components/Newsletter.jsx
+++ src/Components/Home/Components/Newsletter.jsx
@@ -1,46 +1,20 @@
 import React from "react";
 import "./Style/Newsletter.css";
 import { Button, TextField } from "@mui/material";
+import EmailIcon from '@mui/icons-material/Email';
 const Newsletter = (props) => {
   return (
     <>
-      <div className="container">
-        <div className="row news-container-wrapper">
-          <div className="col-md-6 news-image-container">
-            <img src={props.data.image} alt="" className="news-image" />
-          </div>
-          <div className="col-md-6 newsletter-info">
-            <h1 className="news-info-heading">
-              {props.data.Heading}
-              <br />
-              <b>Newsletter</b>
-            </h1>
-            <p className="news-info-para">{props.data.Para}</p>
-
-            <div className="news-subscription">
-              <TextField
-                className="news-input"
-                label="Enter your Email"
-                variant="standard"
-                fullWidth
-              />
-              <Button
-                variant="contained"
-                className="search-btn"
-                sx={{
-                  background: "#6c4a40",
-                  paddingLeft:"20px",
-                  paddingRight:"20px",
-                  fontSize: "18px",
-                  borderRadius: "10px",
-                  "&:hover": { background: "#6c4a40" },
-                }}
-              >
-                Search
-              </Button>
-            </div>
-          </div>
+    
+      <div className=" newsletter-section">
+       <form action="">
+        <h1 style={{fontFamily:"montserrat"}}>{props.data.Heading}<span>Newsletter</span></h1>
+        <p>{props.data.Para}</p>
+        <div className="email-box">
+            <EmailIcon className="email-icon"/><input type="email" name="" placeholder="Enter your Email" className="newsletter-text-box"/>
+            <button type="button" className="newsletter-btn">{props.data.btn}</button>
         </div>
+       </form>
       </div>
     </>
   );
