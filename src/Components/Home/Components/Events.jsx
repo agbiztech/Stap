731692ac6732c3d242b5baeea5e5c3261c803ecf
sakshya25 <2573st@gmail.com>
2023-06-04 17:02:35 +0530

    event card completed

--- src/Components/Home/Components/Events.jsx
+++ src/Components/Home/Components/Events.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./Style/Events.css";
+import "./Style/Events.css";
 import "./Style/Slider.css";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import { Button, TextField } from "@mui/material";
@@ -7,6 +7,9 @@ import "swiper/swiper-bundle.min.css";
 import { FreeMode, Navigation } from "swiper";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import BorderColorIcon from "@mui/icons-material/BorderColor";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import TimelapseIcon from "@mui/icons-material/Timelapse";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 const Events = (props) => {
   return (
     <>
@@ -14,65 +17,88 @@ const Events = (props) => {
         <h1>Top Event For You</h1>
       </Container>
       <Container className="mb-5">
-        
-          <Swiper
-            slidesPerView={3}
-            spaceBetween={30}
-            // pagination={{
-            //   clickable: true,
-            // }}
-            navigation={true}
-            modules={[Navigation]}
-            className="mySwiper"
-            breakpoints={{
-              0: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-              },
-              756: {
-                slidesPerView: 2,
-                spaceBetween:20,
-              },
-              992: {
-                slidesPerView: 3,
-              },
-            }}
-          >
-            {props.data.map((item, index) => (
-              <SwiperSlide key={index}>
-                <Card>
-                  <Card.Img src={item.image} variant="top" />
-                  <Card.Body>
-                    <Card.Subtitle className="mb-3 text-muted d-flex  align-items-center blogDate">
-                      {" "}
-                      <BorderColorIcon style={{ scale: "0.6" }} />{" "}
-                      {item.duration}
-                    </Card.Subtitle>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          // pagination={{
+          //   clickable: true,
+          // }}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+            756: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            992: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          {props.data.map((item, index) => (
+            <SwiperSlide key={index}>
+              <Card className="shadow p-3 mb-5 bg-white rounded">
+                <Card.Img src={item.image} variant="top" />
+                <Card.Body>
+                  <div className="d-flex align-items-end  justify-content-between">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <Card.Title className="tour-title mb-2">
+                        {item.place}
+                      </Card.Title>
+                    </div>
+                    <div className="d-flex justify-content-between align-items-end">
+                      <BookmarkIcon className="d-flex align-items-end justify-content-between bg-light" />
+                    </div>
+                  </div>
 
-                    <Card.Title className="my-4">{item.place}</Card.Title>
-                    <Card.Text className="blogDescription my-4">
-                      {item.placeDes}
+                  <Card.Subtitle className="pt-2 text-muted d-flex  align-items-center tour-info">
+                    {" "}
+                    {item.placeDes}
+                  </Card.Subtitle>
+                  <hr />
+                  <Card.Subtitle className=" text-muted d-flex  align-items-center justify-content-between tour-info">
+                    <div className="d-flex justify-content-between align-items-center event-duration">
+                      <TimelapseIcon  className="mx-1" />
+                      {item.duration}
+                    </div>
+                    <div className="d-flex justify-content-between align-items-center event-capacity">
+                      <AccountCircleIcon className="mx-1"/>
+                      {item.capacity}
+                    </div>
+                  </Card.Subtitle>
+                  <div className="d-flex  align-items-center justify-content-between my-3 mb-0 ">
+                    <Card.Text className="my-4">
+                      <div className="d-flex justify-content-between align-items-center event-price">
+                        ${item.price}
+                      </div>
                     </Card.Text>
-                    <Button
-                      variant="contained"
-                      className="search-btn"
-                      sx={{
-                        background: "#399f6e",
-                        paddingLeft: "20px",
-                        paddingRight: "20px",
-                        fontSize: "18px",
-                        borderRadius: "10px",
-                        "&:hover": { background: "#6c4a40" },
-                      }}
-                    >
-                      {item.btn}
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        
+                    <div className="d-flex justify-content-between align-items-cente">
+                      <Button
+                        variant="contained"
+                        className="search-btn"
+                        sx={{
+                          background: "#399f6e",
+                          paddingLeft: "20px",
+                          paddingRight: "20px",
+                          fontSize: "15px",
+                          borderRadius: "10px",
+                          "&:hover": { background: "#6c4a40" },
+                        }}
+                      >
+                        {item.btn}
+                      </Button>
+                    </div>
+                  </div>
+                </Card.Body>
+              </Card>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </Container>
     </>
   );
