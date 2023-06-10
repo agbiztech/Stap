4bc704904aa05b268922b57b424c94c8a7cbbea9
roshan.prajapati <roshanprajapati678@gmail.com>
2023-06-10 19:48:39 +0530

    done

--- src/Components/AboutUs/Components/AboutUsSection.jsx
+++ src/Components/AboutUs/Components/AboutUsSection.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-
 import "./Style/AboutUsSection.css";
 import AboutUsCard from "./AboutUsCard";
 
 const AboutUsSection = (props) => {
   return (
-    <div className="container-fluid   py-5 px-3">
+    <div className="container-fluid py-5 px-3">
       <Row className="gy-3">
-        <Col className=" col-lg-6">
-          <div className=" ms-sm-4 contentContainer">
+        <Col className="col-lg-6">
+          <div className="ms-sm-4 contentContainer">
             <h1 className="mb-4">More About Us</h1>
             <Row className="g-lg-4 g-4 g-sm-3">
               {props.data.map((item, index) => (
@@ -20,7 +19,7 @@ const AboutUsSection = (props) => {
             </Row>
           </div>
         </Col>
-        <Col className="col-sm-12 col-lg-6">
+        <Col className="col-12 col-lg-6">
           <img
             src="https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?cs=srgb&dl=pexels-fabian-wiktor-994605.jpg&fm=jpg"
             className="img-fluid rounded d-none d-md-block aboutUsImage"
